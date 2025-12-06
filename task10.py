e6d321c6e0df70a16963876fe2ec08bb1b9c0f8d
Abdujalol <Donitursunov98@gmail.com>
2025-12-07 00:33:03 +0500

    10-mashq

---
+++ task10.py
@@ -0,0 +1,14 @@
+ball= int(input('Olgan balingizni kiriting: '))
+
+if 90 <= ball <= 100: 
+    print('A')
+elif 80 <= ball <= 89:
+    print('B')
+elif 70 <= ball <= 79:
+    print('C')
+elif 60 <= ball <= 69:
+    print('D')
+elif 0 <= ball <= 59:
+    print('F')
+elif ball > 100:
+    print('ball mavjud emas')
